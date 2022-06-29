773be913c894f1355065833b89ad016a904cd00d
profflaviomoreni <profflavio.moreni@fiap.com.br>
2022-06-29 20:33:30 -0300

    commit aula 01

---
+++ Controllers/HelpController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.AspNet3.Controllers
+{
+    public class HelpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
