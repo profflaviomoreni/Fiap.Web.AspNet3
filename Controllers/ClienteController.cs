d862b234e1409eba9a2cb80381f3aa7c7b30c82c
profflaviomoreni <profflavio.moreni@fiap.com.br>
2022-07-04 20:40:08 -0300

    Inserindo a funcionalidade de lista e cadastro de cliente

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Fiap.Web.AspNet3.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Fiap.Web.AspNet3.Controllers
 {
@@ -8,7 +9,63 @@ namespace Fiap.Web.AspNet3.Controllers
         [HttpGet]
         public IActionResult Index()
         {
+            // Simulando uma busca no banco de dados
+            
+            var listaClientes = new List<ClienteModel>();
+            
+            listaClientes.Add(new ClienteModel {
+                ClienteId = 1,
+                Nome = "Flávio",
+                Email = "fmoreni@gmail.com",
+                DataNascimento = DateTime.Now,  
+                Observacao = "OBS1"
+            });
+            listaClientes.Add(new ClienteModel
+            {
+                ClienteId = 2,
+                Nome = "Eduardo",
+                Email = "eduardo@gmail.com",
+                DataNascimento = DateTime.Now,
+                Observacao = "OBS3"
+            });
+            listaClientes.Add(new ClienteModel
+            {
+                ClienteId = 3,
+                Nome = "Moreni",
+                Email = "moreni@gmail.com",
+                DataNascimento = DateTime.Now,
+                Observacao = "OBS3"
+            });
+            listaClientes.Add(new ClienteModel
+            {
+                ClienteId = 4,
+                Nome = "Luan",
+                Email = "luan@gmail.com",
+                DataNascimento = DateTime.Now,
+                Observacao = "OBS4"
+            });
+
+
+            return View(listaClientes);
+        }
+
+        [HttpGet]
+        public IActionResult Novo()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Novo(ClienteModel clienteModel)
+        {
+            // Recuperar as informações do cliente digitado
+
+            // Cadastrar no banco de dados (Fake)
+            // bancoDados.Cliente.Save(clienteModel);
+
+            // Exibir uma tela de sucesso. OK
+
+            return View("Sucesso");
         }
 
 
---
+++ Models/ClienteModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Fiap.Web.AspNet3.Models
+{
+    public class ClienteModel
+    {
+
+        public int ClienteId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public string Observacao { get; set; }
+
+    }
+}
