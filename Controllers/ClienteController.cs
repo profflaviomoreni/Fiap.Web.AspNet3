ec49c3eab19e5dea6c63edcf9571919f9c1c796b
profflaviomoreni <profflavio.moreni@fiap.com.br>
2022-07-04 22:05:18 -0300

    fluxo de edição e mensagem na index

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -65,7 +65,109 @@ namespace Fiap.Web.AspNet3.Controllers
 
             // Exibir uma tela de sucesso. OK
 
+            TempData["mensagem"] = "Cliente cadastrado com sucesso";
+
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            //var clienteModel = bancoDados.Cliente.Search(id);
+            var clienteModel = new ClienteModel();
+
+            if (id == 1)
+            {
+                clienteModel = new ClienteModel
+                {
+                    ClienteId = 1,
+                    Nome = "Flavio",
+                    Email = "fmoreni@gmail.com",
+                    DataNascimento = DateTime.Now,
+                    Observacao = "OBS1"
+                };
+            }
+            else if (id == 2)
+            {
+                clienteModel = new ClienteModel
+                {
+                    ClienteId = 2,
+                    Nome = "Eduardo",
+                    Email = "eduardo@gmail.com",
+                    DataNascimento = DateTime.Now,
+                    Observacao = "OBS3"
+                };
+            }
+            else 
+            {
+                clienteModel = new ClienteModel
+                {
+                    ClienteId = 3,
+                    Nome = "Moreni",
+                    Email = "moreni@gmail.com",
+                    DataNascimento = DateTime.Now,
+                    Observacao = "OBS3"
+                };
+            }
+
+
+            return View(clienteModel);
+        }
+
+
+        
+
+
+        [HttpPost]
+        public IActionResult Editar(ClienteModel clienteModel)
+        {
             return View("Sucesso");
+        }
+
+
+
+        [HttpGet]
+        public IActionResult Detalhe(int id)
+        {
+            var clienteModel = new ClienteModel();
+
+            if (id == 1)
+            {
+                clienteModel = new ClienteModel
+                {
+                    ClienteId = 1,
+                    Nome = "Flavio",
+                    Email = "fmoreni@gmail.com",
+                    DataNascimento = DateTime.Now,
+                    Observacao = "OBS1"
+                };
+            }
+            else if (id == 2)
+            {
+                clienteModel = new ClienteModel
+                {
+                    ClienteId = 2,
+                    Nome = "Eduardo",
+                    Email = "eduardo@gmail.com",
+                    DataNascimento = DateTime.Now,
+                    Observacao = "OBS3"
+                };
+            }
+            else
+            {
+                clienteModel = new ClienteModel
+                {
+                    ClienteId = 3,
+                    Nome = "Moreni",
+                    Email = "moreni@gmail.com",
+                    DataNascimento = DateTime.Now,
+                    Observacao = "OBS3"
+                };
+            }
+
+
+            return View(clienteModel);
         }
 
 
